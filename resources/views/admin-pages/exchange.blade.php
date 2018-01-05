9a9911b1281ce8a9f1016e9cfae016458e23402d
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2018-01-05 12:30:28 +0100

    updates added, admin exchange

--- resources/views/admin-pages/exchange.blade.php
+++ resources/views/admin-pages/exchange.blade.php
@@ -12,6 +12,63 @@
 			<h1>Today's Exchange</h1>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-md-9">
+			<div class="lead text-center">
+				<p>Today's Exchange </p>
+				<button class="btn btn-default">
+					<i class="fa fa-database"></i> BTT to 1USD: <i class="fa fa-usd"></i> 
+				</button>
+				<button class="btn btn-default">
+					<i class="fa fa-database"></i> BTT to 1BTC: <i class="fa fa-btc"></i> 
+				</button>
+				<button class="btn btn-default">
+					<i class="fa fa-database"></i> BTT to 1ETH: <i class="fa fa-database"></i> 
+				</button>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="lead text-center">
+				<p>Today's Trading Rate </p>
+				<div class="small">
+					<form class="btt-usd-rate" method="post" onsubmit="return setRate('btt-usd')">
+						<div class="form-group">
+							<label>BTT to USD</label>
+							<input type="number" name="rate" maxlength="15" required="" class="form-control">
+						</div>
+						<div class="form-group">
+							<button class="btn btn-default">Set Today's Rate</button>
+						</div>
+					</form>
+				</div>
+				<hr />
+				<div class="small">
+					<form class="btt-usd-rate" method="post" onsubmit="return setRate('btt-usd')">
+						<div class="form-group">
+							<label>BTT to BTC</label>
+							<input type="number" name="rate" maxlength="15" required="" class="form-control">
+						</div>
+						<div class="form-group">
+							<button class="btn btn-default">Set Today's Rate</button>
+						</div>
+					</form>
+				</div>
+				<hr />
+				<div class="small">
+					<form class="btt-usd-rate" method="post" onsubmit="return setRate('btt-usd')">
+						<div class="form-group">
+							<label>BTT to ETH</label>
+							<input type="number" name="rate" maxlength="15" required="" class="form-control">
+						</div>
+						<div class="form-group">
+							<button class="btn btn-default">Set Today's Rate</button>
+						</div>
+					</form>
+				</div>
+				<hr />
+			</div>
+		</div>
+	</div>
 @endsection
 
 
